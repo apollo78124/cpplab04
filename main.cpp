4a2f5ec246ed0888dccbeb680460ac4f2001417d
apollo78124 <98apollo@naver.com>
2018-10-12 23:52:23 -0700

    a

--- main.cpp
+++ main.cpp
@@ -5,24 +5,36 @@ using namespace std;
 
 int main()
 {
-    Animal *a1 = new Animal();
-    Animal *b1 = new Bird();
-    Animal *c1 = new Canine();
+    Animal a{7, 8, 9};
+    Bird b{34, 12, 34, 56};
+    Canine c{12, 52, 12};
+    Animal *a1 = &a;
+    Animal *a2 = &b;
+    Animal *a3 = &c;
 
-    cout << *(Bird*) b1 << endl;
+    cout << "\n\n";
+    cout << *a1;
+    a1->sleep();
+    a1->eat();
+    a1->move(4, 5);
+    cout << *a1;
 
-    **b1->move(3.1, 4.1, 5.1);
-    /**
-  *c1.move(3.1, 4.1, 5.1);
+    cout << "\n\n";
+    cout << *a2;
+    a2->sleep();
+    a2->eat();
 
-  *a1.eat();
-  *(Bird*) b1.eat();
-  *c1.eat();
+    dynamic_cast<Bird *>(a2)->move(1, 5, 7);
+    cout << *a2;
 
-  *a1.sleep();
-  *b1.sleep();
-  *c1.sleep();
+    cout << "\n\n";
+    cout << *a3;
+    a3->sleep();
+    a3->eat();
+    a3->move(1, 5);
+    dynamic_cast<Canine *>(a3)->hunt();
+    cout << *a3;
+    cout << "\n\n";
 
-  c1.hunt();
-  */
+    return 0;
 }
