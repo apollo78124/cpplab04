f8c2316af6c6fd5f995b0887766db997fc25575a
apollo78124 <98apollo@naver.com>
2018-10-12 19:36:56 -0700

    a

--- main.cpp
+++ main.cpp
@@ -2,16 +2,8 @@
 
 using namespace std;
 
-class container
-{
-private:
-public:
-    container(){ cout << "constructed" << endl;}
-    ~container(){ cout << "destroyed" << endl;}
-};
-
 int main()
 {
-    container first;
+
 
 }
